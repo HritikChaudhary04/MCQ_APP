4e830cdc1bccbf74dd3a5f20c832b68ccf8c2904
Hritik Chaudhary <kitirhhritik@gmail.com>
2025-07-08 22:16:27 +0545

    nothing important

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from datetime import datetime
 
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
